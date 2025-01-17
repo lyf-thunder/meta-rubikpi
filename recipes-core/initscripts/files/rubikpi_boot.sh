27d8317ee160a6be1eb1e6e38a9aaf6bfa030aa4
gongzg0715 <gongzg0715@thundersoft.com>
2025-01-17 16:19:46 +0800

    LED: Fix the LED flashing and the first frame being too bright [2/2]
    Fix the LED flashing and the first frame being too bright
    
    IssueID: RUBIKPI-376
    
    Change-Id: Idcda51d5e49485ac95fd1f8a10aa01fddf3d91fe

--- recipes-core/initscripts/files/rubikpi_boot.sh
+++ recipes-core/initscripts/files/rubikpi_boot.sh
@@ -5,7 +5,3 @@ echo 500 > /sys/bus/platform/devices/a600000.usb/power/autosuspend_delay_ms
 echo 500 > /sys/bus/platform/devices/8c00000.usb/power/autosuspend_delay_ms
 echo auto > /sys/bus/platform/devices/a600000.usb/power/control
 echo auto > /sys/bus/platform/devices/8c00000.usb/power/control
-
-# heartbeat led
-echo heartbeat > /sys/class/leds/green/trigger
-echo 5 > /sys/class/leds/green/brightness
